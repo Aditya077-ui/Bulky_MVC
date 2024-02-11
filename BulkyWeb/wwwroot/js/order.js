eae8b7b90d3137f20bb231a0ad60f9a7700e6f61
Aditya khadka <adityakhadka907@gmail.com>
2024-02-11 19:27:31 +0545

    modified order details

--- BulkyWeb/wwwroot/js/order.js
+++ BulkyWeb/wwwroot/js/order.js
@@ -1,16 +1,13 @@
-﻿$(document).ready(function () {
-    if ($('#tblOrderData').length) {
-        loadDataTable();
-    } else {
-        console.error('#tblOrderData not found.');
-    }
+﻿
+$(document).ready(function () {
+    loadDataTable();
 });
 
 function loadDataTable() {
     dataTable = $('#tblOrderData').DataTable({
         "ajax": { url: '/admin/order/getall' },
         "columns": [
-            { data: 'id', "width": "5%" },
+            { data: 'id', "width": "25%" },
             { data: 'name', "width": "15%" },
             { data: 'phoneNumber', "width": "10%" },
             { data: 'applicationUser.email', "width": "15%" },
@@ -19,14 +16,16 @@ function loadDataTable() {
             {
                 data: 'id',
                 "render": function (data) {
-                    return `<div class="w-75 btn-group" role="group">
-                        <a href="/admin/order/Details?orderId=${data}" class="btn btn-primary mx-2">
-                            <i class="bi bi-pencil-square"></i>
-                        </a>
-                    </div>`;
+                    return `<div class = "w-75 btn-group" role="group">
+               <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Edit</a>
+               
+               </div>`
                 },
                 "width": "25%"
             }
         ]
     });
 }
+
+
+
